b4f867bd83f2e4e33d1723e766dfbcccbed5ec31
kmwestmi <90360068+kmwestmi@users.noreply.github.com>
2021-09-08 20:13:43 -0400

    Add files via upload
    Our first recitation file.

---
+++ Recitation 1.py
@@ -0,0 +1,20 @@
+bunny=10.4
+print(bunny*5)
+
+my_string='hi,how are you'
+print(my_string)
+
+print(type(bunny))
+
+points_obtained=40
+total_points=50
+percentage=(points_obtained/total_points)*100
+print(percentage)
+
+x='I am a UB Student','Krissy','Bunny'
+print(x[0])
+print(x[1])
+print(x[-1])
+
+y='Turtle'
+print(y[3])
